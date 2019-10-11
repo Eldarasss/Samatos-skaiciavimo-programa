41756595ce42325a70aa7fa0759a18cb87aacba2
Eldarasss <55846999+Eldarasss@users.noreply.github.com>
2019-10-11 20:11:11 +0300

    Pridejau kelias funkcijas

--- dictionary.py
+++ dictionary.py
@@ -6,7 +6,7 @@ darbai = {
     'tinkavimas': '7'
 }
 """
-
+"""
 pamatas = 60
 murijimas = 500
 stogas = 120
@@ -28,3 +28,18 @@ uzsakymo_kiekis = input('Suveskite uzsakymo kieki: ')
 uzsakymo_vieneto_kaina = input('Suveskite vieneto kaina: ')
 print(f'Darbu kaina yra: {darbo_kainos_skaiciavimas(uzsakymo_kiekis, uzsakymo_vieneto_kaina)}')
 
+darbo_tipas = input('Pasirinkite viena is siu darbu: pamatas, murijimas, stogas, tinkavimas, dazymas: ')
+"""
+def pamato_skaiciavimas():
+    beginis_metras = input('Iveskite beginiu metru kieki: ')
+    pamato_kaina = input('Iveskite metro kaina ')
+    kaina = int(beginis_metras) * int(pamato_kaina)
+    print(f'Pamato kaina yra: {kaina}')
+
+def murinijimo_skaiciavimas():
+    perimetro_ilgis = input('Iveskite perimetro ilgi: ')
+    aukstis = input('Iveskite auksti: ')
+    kaina_kvadrato = input('Iveskite kvadrato kaina: ')
+    kaina = int(perimetro_ilgis) * int(aukstis) * int(kaina_kvadrato)
+    print(f'Murinijimo kaina yra: {kaina}')
+murinijimo_skaiciavimas()
