26bc9d8969f9d8ff1746378cf3323b4d7ad822c0
Eldarasss <55846999+Eldarasss@users.noreply.github.com>
2019-10-15 15:58:34 +0300

    Add last def function

--- dictionary.py
+++ dictionary.py
@@ -1,33 +1,4 @@
 """
-darbai = {
-    'pamatas': '40',
-    'murinimas': '8',
-    'stogas': '35',
-    'tinkavimas': '7'
-}
-"""
-"""
-pamatas = 60
-murijimas = 500
-stogas = 120
-tinkavimas = 650
-dazymas = 650
-
-
-pamatas_kaina_beg_m = 40
-murijimas_kaina_m2 = 8
-stogas_kaina_m2 = 35
-tinkavimas_kaina_m2 = 7
-dazymas_kaina_m2 = 10
-
-def darbo_kainos_skaiciavimas(darbo_tipas, kvadratura):
-    darbo_kaina = int(darbo_tipas) * int(kvadratura)
-    return darbo_kaina
-
-uzsakymo_kiekis = input('Suveskite uzsakymo kieki: ')
-uzsakymo_vieneto_kaina = input('Suveskite vieneto kaina: ')
-print(f'Darbu kaina yra: {darbo_kainos_skaiciavimas(uzsakymo_kiekis, uzsakymo_vieneto_kaina)}')
-
 darbo_tipas = input('Pasirinkite viena is siu darbu: pamatas, murijimas, stogas, tinkavimas, dazymas: ')
 """
 def pamato_skaiciavimas():
@@ -42,4 +13,33 @@ def murinijimo_skaiciavimas():
     kaina_kvadrato = input('Iveskite kvadrato kaina: ')
     kaina = int(perimetro_ilgis) * int(aukstis) * int(kaina_kvadrato)
     print(f'Murinijimo kaina yra: {kaina}')
-murinijimo_skaiciavimas()
+
+def stogo_skaiciavimas():
+    ilgis = input('Iveskite ilgi: ')
+    plotis = input('Iveskite ploti: ')
+    kaina_kvadrato = input('Iveskite kvadrato kaina')
+    kaina = int(ilgis) * int(plotis) * int(kaina_kvadrato)
+    print(f'Stogo kaina yra: {kaina}')
+
+def tinkavimo_skaiciavimas():
+    ilgis = input('Iveskite ilgi: ')
+    plotis = input('Iveskite ploti: ')
+    kaina_kvadrato = input('Iveskite kvadrato kaina: ')
+    kaina = int(ilgis) * int(plotis) * int(kaina_kvadrato)
+    print(f'Tinkavimo kaina yra: {kaina}')
+
+def dazymo_skaiciavimas():
+    ilgis = input('Iveskite ilgi: ')
+    plotis = input('Iveskite ploti: ')
+    kaina_kvadrato = input('Iveskite kvadrato kaina: ')
+    kaina = int(ilgis) * int(plotis) * int(kaina_kvadrato)
+    print(f'Dazymo kaina yra: {kaina}')
+
+def darbo_tipas():
+    pamatas = pamato_skaiciavimas
+    muras = murinijimo_skaiciavimas
+    stogas = stogo_skaiciavimas
+    tinkas = stogo_skaiciavimas
+    dazymas = dazymo_skaiciavimas
+    print(f'Pasirinkite viena is siu darbu: pamatas, murijimas, stogas, tinkavimas, dazymas: {pamatas}, {muras}, {stogas}, {tinkas}, {dazymas}')
+darbo_tipas()
